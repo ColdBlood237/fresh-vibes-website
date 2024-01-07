aa52ecede60fc72e9bc7f074217d41ff7b3e7219
Pixel <petngalan@gmail.com>
2024-01-07 20:32:45 +0100

    Set up tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Vote from "./pages/Vote";
 import NoPage from "./pages/NoPage";
 
 function App() {
-  return <h1>HOME</h1>;
+  return <h1 className="text-9xl red font-bold underline">HOME</h1>;
 }
 
 export default App;
