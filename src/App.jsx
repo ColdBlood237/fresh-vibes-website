6f7c6627bd9e918323ff85cbdd295b6b5354383a
Pixel <petngalan@gmail.com>
2024-01-07 20:20:40 +0100

    Set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import Upload from "./pages/Upload";
 import Vote from "./pages/Vote";
 import NoPage from "./pages/NoPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/vote" element={<Vote />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>HOME</h1>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Upload from "./pages/Upload";
+import Vote from "./pages/Vote";
+import NoPage from "./pages/NoPage";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "upload",
+    element: <Upload />,
+  },
+  {
+    path: "vote",
+    element: <Vote />,
+  },
+  {
+    path: "*",
+    element: <NoPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
